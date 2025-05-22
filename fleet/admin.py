492523fd8f2e7b18880f6551aaee8bbcc9542f17
Rhriti <hritijrana07@gmail.com>
2025-05-22 21:51:07 +0530

    admin functionality added

--- fleet/admin.py
+++ fleet/admin.py
@@ -1,3 +1,20 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from .models import Station, Vehicle
+
+class StationResource(resources.ModelResource):
+    class Meta:
+        model = Station
+        import_id_fields = ['station_code']
+        fields = ['station_name', 'station_code', 'latitude', 'longitude', 'city', 'operating_radius']
+
+@admin.register(Station)
+class StationAdmin(ImportExportModelAdmin):
+    resource_class = StationResource
+    list_display = ('station_name', 'station_code', 'city', 'operating_radius')
+
+@admin.register(Vehicle)
+class VehicleAdmin(ImportExportModelAdmin):
+    list_display = ('registration_number', 'vehicle_model', 'current_station', 'odometer_reading', 'fuel_level')
